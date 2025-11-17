2d6ec272657fe42e0b7757832398770b1126e515
Andrii <rik24096@gmail.com>
2025-11-17 15:46:18 +0100

    Add Expense class with constructor
    getters, setters, and toString

---
+++ src/main/java/cz/upce/fei/Expense.java
@@ -0,0 +1,53 @@
+package cz.upce.fei;
+
+public class Expense {
+    private String date; // дата
+    private String category; // категорія
+    private String note; // коментар
+    private double amount; // сума витрати
+
+    public Expense(String date, String category, String note, double amount) {
+        System.out.println("Expense object created");
+        this.date = date;
+        this.category = category;
+        this.note = note;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Date: " + date + " | Category: " + category + " | Note: " + note + " | Amount: " + amount + "Kc";
+    }
+}
--- src/main/java/cz/upce/fei/Main.java
+++ src/main/java/cz/upce/fei/Main.java
@@ -3,6 +3,9 @@ package cz.upce.fei;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Main started");
+        Expense expense1 = new Expense("17.11.2025", "Food", "FastFood", 50.67);
+
+        System.out.println("Expense 1: " + expense1);
     }
 }
